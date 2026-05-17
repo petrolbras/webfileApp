52e525ffedbbafcbe8edd12411d2b330d7b32829
petrol <pietrogouvearepleop@gmail.com>
2026-05-17 20:32:33 -0300

    Add Svelte layout file

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<svelte:options runes={true} />
+
+<script lang="ts">
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children()}
