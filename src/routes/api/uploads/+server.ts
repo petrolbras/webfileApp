87f5cb1fd5fb289a7fdb6f0ab0e60e4f38ec0110
petrol <pietrogouvearepleop@gmail.com>
2026-05-24 18:18:29 -0300

    fixed wrong path for storage

--- src/routes/api/uploads/+server.ts
+++ src/routes/api/uploads/+server.ts
@@ -6,7 +6,7 @@ import { SanitizeName } from '$lib/SanitizeName';
 import { json } from '@sveltejs/kit';
 import { GetAvailableName } from '$lib/getAvailableName';
 
-const ROOT_DIR = path.resolve('storage');
+const STORAGE_DIR = path.resolve('storage');
 const MAX_FILE_SIZE = 100 * 1024 * 1024; // 100 MB
 
 export async function POST({request}) {
@@ -16,8 +16,8 @@ export async function POST({request}) {
         const file = formData.get('file');
 
         const relativepath = (formData.get('path') as string) || '';
-        const resolvedRoot = path.resolve(ROOT_DIR);
-        const destinationDir = path.resolve(path.join(ROOT_DIR, relativepath));
+        const resolvedRoot = path.resolve(STORAGE_DIR);
+        const destinationDir = path.resolve(path.join(STORAGE_DIR, relativepath));
         const resolvedDestination = path.resolve(destinationDir);
     
         if (!resolvedDestination.startsWith(resolvedRoot)) {
@@ -51,6 +51,8 @@ export async function POST({request}) {
             writeStream.on('finish', resolve);
             writeStream.on('error', reject);
         });
+
+        console.log("Saving to:", finalFilePath);
 
         return json({
             message: 'File uploaded successfully',
